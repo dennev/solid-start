69dbc497b52e05387d2b763506e1f5dcd1aab977
dennev <221695162+dennev@users.noreply.github.com>
2025-08-06 00:54:58 +0900

    fix: remove `$ServerOnly` directive from notes example

--- examples/notes/src/components/SearchField.tsx
+++ examples/notes/src/components/SearchField.tsx
@@ -12,7 +12,7 @@ export default function SearchField() {
   const [isSearching] = useTransition();
   const [search, setParams] = useSearchParams();
   return (
-    <form class="search" role="search" onSubmit={e => e.preventDefault()} $ServerOnly>
+    <form class="search" role="search" onSubmit={e => e.preventDefault()}>
       <label class="offscreen" for="sidebar-search-input">
         Search for a note by title
       </label>
